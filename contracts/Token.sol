6e7274994de360cc2858260aa8d54e9b50d03d03
Mike Piehl <piehlm@yahoo.com>
2024-02-27 15:27:11 -0500

    6. Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,10 +10,16 @@ contract Token {
 	uint256 public totalSupply;
 
 	mapping (address => uint256) public balanceOf;
+	mapping (address => mapping (address => uint256)) public allowance;
 
 	event Transfer(
 		address indexed from, 
 		address indexed to, 
+		uint256 value
+	);
+	event Approval(
+		address indexed owner,
+		address indexed spender,
 		uint256 value
 	);
 	
@@ -36,6 +42,32 @@ contract Token {
 		balanceOf[_to] = balanceOf[_to] + _value;
 
 		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
+	function approve(address _spender, uint256 _value) 
+		public 
+		returns(bool success) 
+	{
+		require(_spender != address(0));
+		
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) 
+		public 
+		returns (bool success) 
+	{
+		
+		require (balanceOf[_from] >= _value);
+		require (_to != address(0));
+		
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from, _to, _value);
 		return true;
 	}
 	
