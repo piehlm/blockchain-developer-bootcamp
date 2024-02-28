cb7862243eac20f3300ae99705174e5a86debf0e
Mike Piehl <piehlm@yahoo.com>
2024-02-28 11:42:01 -0500

    7.Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -33,15 +33,18 @@ contract Token {
 		totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
+	function _transfer(address _from, address _to, uint256 _value) internal {
+		require (_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from, _to, _value);		
+	}
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		
 		require (balanceOf[msg.sender] >= _value);
-		require (_to != address(0));
-		
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
-
-		emit Transfer(msg.sender, _to, _value);
+		_transfer(msg.sender, _to, _value);
 		return true;
 	}
 	function approve(address _spender, uint256 _value) 
@@ -59,15 +62,13 @@ contract Token {
 	function transferFrom(address _from, address _to, uint256 _value) 
 		public 
 		returns (bool success) 
-	{
-		
+	{	
 		require (balanceOf[_from] >= _value);
-		require (_to != address(0));
-		
-		balanceOf[_from] = balanceOf[_from] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
+		require(_value <= allowance[_from][msg.sender]);
 
-		emit Transfer(_from, _to, _value);
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+		_transfer(_from, _to, _value);
+		
 		return true;
 	}
 	
