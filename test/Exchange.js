13b3361bb90b343520ffd4fa152d5c7545baf76c
Mike Piehl <piehlm@yahoo.com>
2024-03-04 13:08:44 -0500

    10. Withdraw Tokens

--- test/Exchange.js
+++ test/Exchange.js
@@ -72,4 +72,58 @@ describe('Exchange', ()=>{
 
 		})
 	})
+
+	describe('Withdraw Tokens', () => {
+		let transaction, result
+		let amount = tokens(10)
+
+		describe('Success', () => {
+			beforeEach(async () => {
+				transaction = await token1.connect(user1).approve(exchange.address, amount)
+				result = await transaction.wait()
+				transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+				result = await transaction.wait()
+				transaction = await exchange.connect(user1).withdrawToken(token1.address, amount)
+				result = await transaction.wait()				
+			})
+
+			it('withdraws token funds', async () => {
+				expect(await token1.balanceOf(exchange.address)).to.equal(0)
+				expect(await exchange.tokens(token1.address, user1.address)).to.equal(0)
+				expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(0)
+			})
+			it('Emits a Deposit event', async ()=> {
+				const event = result.events[1]
+				expect(event.event).to.equal('Withdraw')
+
+				const args = event.args
+				expect(args.token).to.equal(token1.address)
+				expect(args.user).to.equal(user1.address)
+				expect(args.amount).to.equal(amount)
+				expect(args.balance).to.equal(0)
+			})
+		})
+
+		describe('Failure', () => {
+			it('fails when insufficient balance', async () => {
+				await expect(exchange.connect(user1).withdrawToken(token1.address, amount)).to.be.reverted
+			})
+
+		})
+	})
+	describe('Checking Token Balances', () => {
+		let transaction, result
+		let amount = tokens(1)
+
+		beforeEach(async () => {
+			transaction = await token1.connect(user1).approve(exchange.address, amount)
+			result = await transaction.wait()
+			transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+			result = await transaction.wait()
+		})
+
+		it('returns user balance', async () => {
+			expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+		})
+	})
 })
