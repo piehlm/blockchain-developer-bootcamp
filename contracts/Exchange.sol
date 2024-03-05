596eaa30f29f80ef567e7861cc5841533f885ab2
Mike Piehl <piehlm@yahoo.com>
2024-03-05 14:05:16 -0500

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -7,11 +7,31 @@ import "./Token.sol";
 contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
+	uint256 public orderCount;
+	struct _Order {
+		uint256 id;
+		address user;
+		address tokenGet;
+		uint amountGet;
+		address tokenGive;
+		uint256 amountGive;
+		uint256 timestamp;
+	}
 
 	mapping(address => mapping(address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
+	event Order(		
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -41,4 +61,33 @@ contract Exchange {
 	{	
 		return tokens[_token][_user];
 	}
+
+	function makeOrder(
+		address _tokenGet, 
+		uint256 _amountGet, 
+		address _tokenGive, 
+		uint256 _amountGive
+	) public {
+		require (balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(
+			orderCount, 
+			msg.sender, 
+			_tokenGet, 
+			_amountGet, 
+			_tokenGive, 
+			_amountGive, 
+			block.timestamp
+		);
+		emit Order(
+			orderCount, 
+			msg.sender, 
+			_tokenGet, 
+			_amountGet, 
+			_tokenGive, 
+			_amountGive, 
+			block.timestamp
+		);
+	}	
 }
